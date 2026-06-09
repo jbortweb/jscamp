1de646807ad8bffa51cd02c989583e8b55d084ec
Jbortweb <jbortweb@gmail.com>
2026-06-09 19:57:50 +0200

    refactor: update Resultados to use JobCard and accept jobs prop

--- 02-react/vite/02-react/src/components/Resultados.jsx
+++ 02-react/vite/02-react/src/components/Resultados.jsx
@@ -1,72 +1,26 @@
-import data from '../data.json'
+import JobCard from './JobCard.jsx'
 
-const Resultados = () => {
+const Resultados = ({ jobs }) => {
   return (
     <>
       <section>
         <h2 className="search-result">Resultados de búsqueda</h2>
 
         <div className="jobs-listings">
-          {data.map((job) => (
-            <article
+          {jobs.map((job) => (
+            <JobCard
               key={job.id}
-              className="job-listing-card"
-              modalidad={job.data.modalidad}
-              technology={job.data.technology}
-              nivel={job.data.nivel}
-            >
-              <div>
-                <h3>{job.titulo}</h3>
-                <small>
-                  {job.empresa} | {job.ubicacion}
-                </small>
-                <p>{job.descripcion}</p>
-              </div>
-              <button className="button-apply-job">Aplicar</button>
-            </article>
+              job={job}
+              data-modalidad={job.data.modalidad}
+              data-nivel={job.data.nivel}
+              data-technology={job.data.technology}
+              titulo={job.titulo}
+              empresa={job.empresa}
+              ubicacion={job.ubicacion}
+              descripcion={job.descripcion}
+            ></JobCard>
           ))}
         </div>
-
-        <nav className="pagination">
-          <a href="#">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M15 6l-6 6l6 6" />
-            </svg>
-          </a>
-          <a className="is-active" href="#">
-            1
-          </a>
-          <a href="#">2</a>
-          <a href="#">3</a>
-          <a href="#">4</a>
-          <a href="#">5</a>
-          <a href="#">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M9 6l6 6l-6 6" />
-            </svg>
-          </a>
-        </nav>
       </section>
     </>
   )
